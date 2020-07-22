c4cb27f84c5fbcb2bc1826cc684ab2f853d71f5c
qaz19452 <1204034915@qq.com>
2020-07-22 10:59:41 +0800

    完成首页问题列表

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,22 +1,30 @@
 package com.example.community.controller;
 
+import com.example.community.dto.QuestionDTO;
+import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
+import com.example.community.model.Question;
 import com.example.community.model.User;
+import com.example.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request, Model model){
         Cookie[] cookies = request.getCookies();
         if (cookies!=null && cookies.length!=0) {
             for (Cookie cookie : cookies) {
@@ -30,6 +38,9 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
--- src/main/java/com/example/community/mapper/QuestionMapper.java
+++ src/main/java/com/example/community/mapper/QuestionMapper.java
@@ -3,11 +3,17 @@ package com.example.community.mapper;
 import com.example.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Mapper
 public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator})")
     void create(Question question);
+
+    @Select("select * from question")
+    List<Question> list();
 }
