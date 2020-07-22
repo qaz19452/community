8271d0d3296bc0828cf2d1a4835cc482c086daf5
qaz19452 <1204034915@qq.com>
2020-07-22 18:29:10 +0800

    实现问题列表分页

--- src/main/java/com/example/community/service/QuestionService.java
+++ src/main/java/com/example/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.community.service;
 
+import com.example.community.dto.PaginationDTO;
 import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
@@ -20,9 +21,18 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        page=page<1?1:page;
+        page=page>paginationDTO.getTotalPage()?paginationDTO.getTotalPage():page;
+
+        Integer offset = size*(page-1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -30,7 +40,8 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
+
 }
